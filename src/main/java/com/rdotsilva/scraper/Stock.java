d32687770b4ea4ee64e2c1ba2d434c0748be707b
RdotSilva <RyandotSilva@gmail.com>
2020-02-16 17:23:06 -0500

    Add getters/setters for stock

--- src/main/java/com/rdotsilva/scraper/Stock.java
+++ src/main/java/com/rdotsilva/scraper/Stock.java
@@ -13,4 +13,75 @@ public class Stock {
     private String averageVolume;
     private String marketCap;
 
+    public long id() {
+        return id;
+    }
+
+    public void id(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getScrapeDate() {
+        return scrapeDate;
+    }
+
+    public void setScrapeDate(Timestamp scrapeDate) {
+        this.scrapeDate = scrapeDate;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getLastPrice() {
+        return lastPrice;
+    }
+
+    public void setLastPrice(String lastPrice) {
+        this.lastPrice = lastPrice;
+    }
+
+    public String getChangeAmount() {
+        return changeAmount;
+    }
+
+    public void setChangeAmount(String changeAmount) {
+        this.changeAmount = changeAmount;
+    }
+
+    public String getChangePercent() {
+        return changePercent;
+    }
+
+    public void setChangePercent(String changePercent) {
+        this.changePercent = changePercent;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getAverageVolume() {
+        return averageVolume;
+    }
+
+    public void setAverageVolume(String averageVolume) {
+        this.averageVolume = averageVolume;
+    }
+
+    public String getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(String marketCap) {
+        this.marketCap = marketCap;
+    }
 }
