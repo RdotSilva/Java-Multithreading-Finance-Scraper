87b6741bfdb0cdb6f17e4af4d7ae090d364c071c
RdotSilva <RyandotSilva@gmail.com>
2020-02-16 17:23:38 -0500

    Add Scraper class

---
+++ src/main/java/com/rdotsilva/scraper/Scraper.java
@@ -0,0 +1,19 @@
+package com.rdotsilva.scraper;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Scraper {
+
+    ArrayList<Stock> stockList = new ArrayList<>();
+
+}
