1aa6439df8bb91c48763ecbb64fe797a504f8297
RdotSilva <RyandotSilva@gmail.com>
2020-02-16 17:24:04 -0500

    Add login method

--- src/main/java/com/rdotsilva/scraper/Scraper.java
+++ src/main/java/com/rdotsilva/scraper/Scraper.java
@@ -16,4 +16,19 @@ public class Scraper {
 
     ArrayList<Stock> stockList = new ArrayList<>();
 
+    public static void login(WebDriver driver) {
+        String loginUrl = "https://login.yahoo.com/config/login?.src=fpctx&.intl=us&.lang=en-US&.done=https%3A%2F%2Fwww.yahoo.com";
+        String email = "ryansilva.student@careerdevs.com";
+        String password = "%x_2*xC98H;";
+
+        String loginId = "login-username";
+        String passwordId = "login-passwd";
+
+        driver.get(loginUrl);
+
+        driver.findElement(By.id(loginId)).sendKeys(email + Keys.RETURN);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        driver.findElement(By.id(passwordId)).sendKeys(password + Keys.RETURN);
+    }
+
 }
