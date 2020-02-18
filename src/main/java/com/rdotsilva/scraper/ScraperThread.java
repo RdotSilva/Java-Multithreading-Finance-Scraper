d824c88d06ce64b4c7a5ab4cef9f3451da83023b
RdotSilva <RyandotSilva@gmail.com>
2020-02-17 19:16:59 -0500

    Refactor ScraperThread

--- src/main/java/com/rdotsilva/scraper/ScraperThread.java
+++ src/main/java/com/rdotsilva/scraper/ScraperThread.java
@@ -6,9 +6,9 @@ public class ScraperThread extends Thread {
     @Override
     public void run() {
         System.out.println("Scraper thread one running");
-        Scraper scraper = new Scraper();
+        Scraper scraper = new Scraper("chrome");
         try {
-            scraper.scrape();
+            scraper.scrape(scraper.driver);
             scraper.printStocks();
         } catch (IOException e) {
             e.printStackTrace();
