e87025fb17cd0c1bb767e1e7b4988b3e56fcb145
RdotSilva <RyandotSilva@gmail.com>
2020-02-17 19:17:07 -0500

    Refactor ScraperThreadTwo

--- src/main/java/com/rdotsilva/scraper/ScraperThreadTwo.java
+++ src/main/java/com/rdotsilva/scraper/ScraperThreadTwo.java
@@ -6,9 +6,9 @@ public class ScraperThreadTwo extends ScraperThread {
     @Override
     public void run() {
         System.out.println("Scraper thread two running");
-        Scraper scraper = new Scraper();
+        Scraper scraper = new Scraper("gecko");
         try {
-            scraper.scrape();
+            scraper.scrape(scraper.driver);
             scraper.printStocks();
         } catch (IOException e) {
             e.printStackTrace();
